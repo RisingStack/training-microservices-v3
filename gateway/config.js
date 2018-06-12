69223972790eb4d538953d8fb24001836ce2e97a
Shadowbeetle <tamas.kadlecsik@gmail.com>
2018-06-12 14:59:06 +0200

    fix config

--- gateway/config.js
+++ gateway/config.js
@@ -1,9 +1,14 @@
 const {
   PORT: serverPort,
-  PRODUCTS_API_URL: productsAPIURL = 'http://localhost:3001',
-  USERS_API_URL: usersAPIURL = 'http://localhost:3002',
+  PRODUCT_WEB_SERVICE_HOST,
+  PRODUCT_WEB_SERVICE_PORT,
+  USER_WEB_SERVICE_HOST,
+  USER_WEB_SERVICE_PORT,
   JWT_SECRET: jwtSecret
 } = process.env
+
+const productsAPIURL = `${PRODUCT_WEB_SERVICE_HOST}:${PRODUCT_WEB_SERVICE_PORT}`
+const usersAPIURL = `${USER_WEB_SERVICE_HOST}:${USER_WEB_SERVICE_PORT}`
 
 module.exports = {
   serverPort,
