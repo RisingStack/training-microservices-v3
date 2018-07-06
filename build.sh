6d09cdaa4d0df2bcadc4a78eef32b7a3f186199e
Peter Czibik <p.czibik@gmail.com>
2018-07-06 15:28:27 +0200

    tweak deploy script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+app_name=$1
+image_name=rs-ms/$app_name
+current_version=$(docker images $image_name --format "{{.Tag}}" | head -2 | tail -1 | grep -o "[0-9]\+")
+next_version=$(( $current_version + 1))
+docker build -t $image_name:v$next_version -t $image_name:latest .
+
+kubectl delete pod -l app=$app_name
